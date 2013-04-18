544bc72dc2c6a96e15301f81dc0bc6bbc27aa920
Alexander J Pinkney <azp@cs.nott.ac.uk>
2013-04-18 17:08:35 +0100

    Added grid layout with dumb floats, and multi-column dumb floats

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var VS = 14;
 var PS_PX = PS * PX_PER_PT;
 var VS_PX = VS * PX_PER_PT;
 var TOP_MARGIN = 1.5 * VS_PX;
-var BOT_MARGIN = 2 * TOP_MARGIN;
+var BOT_MARGIN = TOP_MARGIN;
 var PAR_SEP = VS_PX;
 var MIN_GUTTER = 12 * PX_PER_PT;
 var GALLEY_WIDTHS = [];
@@ -16,6 +16,11 @@ var BODYHEIGHT;
 var FLOAT_QUEUE;
 var LINE_QUEUE;
 var CURR_P;
+var CURR_ROW;
+var CURR_COL;
+
+var COLUMNS;
+var NUM_ROWS;
 
 var FLOAT_TYPES = Object.freeze({
     NONE: 0,
@@ -24,25 +29,31 @@ var FLOAT_TYPES = Object.freeze({
     SIMPLEQUEUE: 3
 });
 
+var MULTI_COLUMN = false;
+
 var FLOAT_TYPE;
 
-FLOAT_TYPE = FLOAT_TYPES.SIMPLEQUEUE;
+FLOAT_TYPE = FLOAT_TYPES.DUMB;
 
 function Floatable(width, height) {
     this.width = width;
     this.height = height;
 }
 
-function outputDiv(cssclass, width, height, x, y, text) {
-    $('#main').append('<div class="' + cssclass + '" style="width: ' + width + 'px; height: ' + height + 'px; position: absolute; top: ' + y + 'px; left: ' + x + 'px; font-size: ' + PS_PX + 'px">' + text + '</div>\n');
+function getDiv(cssclass, width, height, text) {
+    return '<div class="' + cssclass + '" style="width: ' + width + 'px; height: ' + height + 'px; font-size: ' + PS_PX + 'px">' + text + '</div>\n';
 }
 
-function outputLine(width, height, x, y, text) {
-    outputDiv("line", width, height, x, y, text);
+function getLine(width, height, text) {
+    return getDiv("line", width, height, text);
 }
 
-function outputFloat(width, height, x, y) {
-    outputDiv("floatable", width, height, x, y, "figure");
+function getFloat(width, height, text) {
+    return getDiv("floatable", width, height, text);
+}
+
+function outputDiv(cssclass, x, y, text) {
+    $('#main').append('<div class="' + cssclass + '" style="position:absolute; top: ' + y + 'px; left: ' + x + 'px">' + text + '</div>');
 }
 
 function nrand() { // thanks to http://www.protonfish.com/random.shtml
@@ -65,6 +76,28 @@ function generatePar(width) {
         }
         $('#main').append('<div class="bgblue" style="width: ' + width + 'em; height: 12pt"></div>');
         size -= width;
+    }
+}
+
+function nextPos() {
+    // Increments CURR_ROW and CURR_COL to next available empty space (possibly the same empty space)
+    var c, r;
+    for (c = CURR_COL; true; c++) {
+        for (r = CURR_ROW; r < NUM_ROWS; r++) {
+            if (Array.isArray(COLUMNS[c])) {
+                if (COLUMNS[c][r] === undefined) {
+                    CURR_COL = c;
+                    CURR_ROW = r;
+                    return;
+                }
+            } else {
+                COLUMNS[c] = [];
+                CURR_COL = c;
+                CURR_ROW = 0;
+                return;
+            }
+        }
+        CURR_ROW = 0;
     }
 }
 
@@ -102,7 +135,11 @@ function enqueueNext(galleywidth, min_badness_index) {
 }
 
 function doLayout() {
-    var i, nc, ex_ws, rq_ws, galleywidth, colsep, curr_x, curr_y, dropdown, pageWidth = window.innerWidth, numCols, badness = [], min_badness_index = 0, name, p, l, w, h, scale;
+    var i, nc, ex_ws, rq_ws, galleywidth, colsep, curr_x, curr_y, dropdown, pageWidth, numCols, badness, min_badness_index, name, p, l, w, h, scale, span;
+
+    pageWidth = window.innerWidth;
+    badness = [];
+    min_badness_index = 0;
 
     BODYHEIGHT = (window.innerHeight - BOT_MARGIN - TOP_MARGIN);
 
@@ -125,22 +162,34 @@ function doLayout() {
     galleywidth = GALLEY_WIDTHS[min_badness_index];
 
     colsep = Math.floor(pageWidth / numCols);
-    curr_x = (colsep - galleywidth) / 2;
-    curr_y = TOP_MARGIN;
 
-    // Shove lines onto screen
+    /*  
+        Previously, we (and I quote) "just shove[d] lines onto screen" -- Now we need to populate the COLUMNS array and then output the contents of that to the screen.
 
-    $('#main').empty(); //clear old stuff
+        So, what goes into the COLUMNS array?
+            - There must be one element for every column in the document
+            - These can be .push()ed on the fly
+            - These are themselves arrays with one element per multiple of the main leading (VS_PX in this case) that will fit in the column
+                - If an element is undefined -- we assume it's available
+                - Otherwise the element will be a string containing the html to output (possibly the empty string)
+                    - the html should specify its own dimensions, but not its position (this will be calculated by its position in the COLUMNS array)
 
-    dropdown = '<select id="dropdown" onchange="updateFloatType(this)">\n';
-    for (name in FLOAT_TYPES) {
-        dropdown += '<option value="' + FLOAT_TYPES[name] + '"' + (FLOAT_TYPE === FLOAT_TYPES[name] ? " selected" : "") + '>' + name + '</option>';
+        Yep, that sounds plausible.
+
+    */
+
+    COLUMNS = [];
+
+    // How many lines? We have (window.innerHeight - TOP_MARGIN - BOT_MARGIN) total space, and each line is VS_PX high. So:
+    NUM_ROWS = Math.floor((window.innerHeight - TOP_MARGIN - BOT_MARGIN) / VS_PX);
+    if (NUM_ROWS < 1) {
+        NUM_ROWS = 1;
     }
-    dropdown += "</select>\n";
 
-    outputDiv("title", 1000, PS_PX + 5, (window.innerWidth - 1000) / 2, 3, dropdown + (FLOAT_TYPE === FLOAT_TYPES.NONE ? "none" : (FLOAT_TYPE === FLOAT_TYPES.DUMB ? "dumb" : (FLOAT_TYPE === FLOAT_TYPES.MSWORD ? "MS Word" : (FLOAT_TYPE === FLOAT_TYPES.SIMPLEQUEUE ? "simple queue" : "unknown (" + FLOAT_TYPE + ")")))) + ", " + numCols + " cols, badness: " + Math.round(badness[min_badness_index] * 100) / 100);
+    CURR_ROW = 0;
+    CURR_COL = 0;
 
-    if (FLOAT_TYPE === FLOAT_TYPES.SIMPLEQUEUE) {
+    if (FLOAT_TYPE === FLOAT_TYPES.SIMPLEQUEUE) {/*
         FLOAT_QUEUE = [];
         LINE_QUEUE = [];
         CURR_P = 0;
@@ -167,52 +216,81 @@ function doLayout() {
                 }
 
             }
-//            curr_y += PAR_SEP;
         }
-
+*/
     } else {
 
         for (p = 0; p < PARAGRAPH_TREE.length; p++) {
-            if (curr_y > (window.innerHeight - BOT_MARGIN)) { //start new column
-                curr_y = TOP_MARGIN;
-                curr_x += colsep;
-            }
 
             if (Array.isArray(PARAGRAPH_TREE[p])) {
                 // We assume it's a normal paragraph
                 for (l = 0; l < PARAGRAPH_TREE[p][min_badness_index].length; l++) {
-                    if (curr_y > (window.innerHeight - BOT_MARGIN)) { //start new column
-                        curr_y = TOP_MARGIN;
-                        curr_x += colsep;
-                    }
-
-                    outputLine(PARAGRAPH_TREE[p][min_badness_index][l], PS_PX, curr_x, curr_y, 'W' + min_badness_index + ' P' + p + ' L' + l + ' Qwertyuiop Lorem ipsum dolor sit amet, consectetur adipiscing elit.');
-
-                    curr_y += VS_PX;
+                    nextPos();
+                    COLUMNS[CURR_COL][CURR_ROW] = getLine(PARAGRAPH_TREE[p][min_badness_index][l], PS_PX, 'W' + min_badness_index + ' P' + p + ' L' + l + ' Qwertyuiop Lorem ipsum dolor sit amet, consectetur adipiscing elit.');
+                }
+                nextPos();
+                if (CURR_ROW !== 0) {
+                    COLUMNS[CURR_COL][CURR_ROW] = "";
                 }
             } else {
                 // It must be a Floatable object
+                if (FLOAT_TYPE === FLOAT_TYPES.NONE) {
+                    continue;
+                }
 
                 // Scale width to fit column (and height proportionately)
                 w = PARAGRAPH_TREE[p].width;
                 h = PARAGRAPH_TREE[p].height;
 
-                scale = galleywidth / w;
-
-                w *= scale;
-                h *= scale;
-
+                // Firstly, scale down the height if it's bigger than the body height
                 if (h > BODYHEIGHT) {
                     scale = BODYHEIGHT / h;
                     w *= scale;
                     h *= scale;
                 }
 
+                // Now have a look at the width and see if it could span any columns
+                span = Math.round(w / galleywidth);
+
+                if (span > numCols) {
+                    span = numCols;
+                }
+
+                if (span < 1 || !MULTI_COLUMN) {
+                    span = 1;
+                }
+
+                scale = (span * galleywidth + (span - 1) * (colsep - galleywidth)) / w;
+
+                w *= scale;
+                h *= scale;
+
                 if (FLOAT_TYPE === FLOAT_TYPES.DUMB) {
 
                     // Just stick the figure out wherever
-                    outputFloat(w, h, curr_x, curr_y);
-                    curr_y += h;
+                    nextPos();
+                    // Now reserve some space for the figure
+                    
+                    // How many lines do we need to reserve?
+                    // Let's say if it's less than n.5,  no need for an extra space
+                    var nlines = Math.round(h / VS_PX) + 1;
+                    for (i = CURR_ROW; i < CURR_ROW + nlines; i++) {
+                        for (j = CURR_COL; j < CURR_COL + span; j++) {
+                            if (!Array.isArray(COLUMNS[j])) {
+                                COLUMNS[j] = [];
+                            }
+                            COLUMNS[j][i] = "";
+                        }
+                    }
+                    
+                    COLUMNS[CURR_COL][CURR_ROW] = getFloat(w, h, "float scaled to " + (Math.round(scale * 10000)) / 100 + "%");
+                    
+                    if (CURR_ROW > 0) { // blank out any lines above
+                        for (i = CURR_COL + 1; i < CURR_COL + span; i++) {
+                            COLUMNS[i][CURR_ROW - 1] = "<!-- foo -->";
+                            console.log('foo');
+                        }
+                    }
 
                 } else if (FLOAT_TYPE === FLOAT_TYPES.MSWORD) {
 
@@ -222,7 +300,7 @@ function doLayout() {
                         curr_x += colsep;
                     }
 
-                    outputFloat(w, h, curr_x, curr_y);
+                    getFloat(w, h, "FLOAT");
                     curr_y += h;
 
                 } else {
@@ -230,14 +308,49 @@ function doLayout() {
                         // console.log(FLOAT_TYPE);
                 }
             }
-            curr_y += PAR_SEP;
+            //curr_y += PAR_SEP;
 
         }
     }
+
+    $('#main').empty(); //clear old stuff
+
+    dropdown = '<select id="dropdown" onchange="updateFloatType(this)">\n';
+    for (name in FLOAT_TYPES) {
+        dropdown += '<option value="' + FLOAT_TYPES[name] + '"' + (FLOAT_TYPE === FLOAT_TYPES[name] ? " selected" : "") + '>' + name + '</option>';
+    }
+    dropdown += "</select>\n";
+
+    $('#main').append('<div class="title" style="position:absolute; width: ' + 1000 + 'px; height: ' + (PS_PX + 5) + 'px; left: ' + ((window.innerWidth - 1000) / 2) + 'px; top: ' + 3 + 'px">' + dropdown + (FLOAT_TYPE === FLOAT_TYPES.NONE ? "none" : (FLOAT_TYPE === FLOAT_TYPES.DUMB ? "dumb" : (FLOAT_TYPE === FLOAT_TYPES.MSWORD ? "MS Word" : (FLOAT_TYPE === FLOAT_TYPES.SIMPLEQUEUE ? "simple queue" : "unknown (" + FLOAT_TYPE + ")")))) + ", " + numCols + " cols, badness: " + Math.round(badness[min_badness_index] * 100) / 100 + ' <label><input id="multi" onclick="toggleMulti();" type="checkbox" ' + (MULTI_COLUMN ? 'checked' : '') + ' > multicolumn?</label></div>');
+
+    // Now we need to output the contents of the COLUMNS array
+    for (p = 0; p < COLUMNS.length; p++) {
+        //console.log("p is " + p);
+        if (!Array.isArray(COLUMNS[p])) {
+            continue;
+        }
+
+        for (l = 0; l < COLUMNS[p].length; l++) {
+            //console.log("\tl is " + l);
+            if (COLUMNS[p][l] === undefined || COLUMNS[p][l] === "") {
+                continue;
+            }
+
+            outputDiv("container", (colsep - galleywidth) / 2 + p * colsep, TOP_MARGIN + l * VS_PX, COLUMNS[p][l]);
+        }
+    }
+    //console.log(COLUMNS);
+
 }
 
 function updateFloatType(dd) {
     FLOAT_TYPE = dd.options[dd.selectedIndex].value;
+    doLayout();
+}
+
+function toggleMulti() {
+    MULTI_COLUMN = !MULTI_COLUMN;
+    $('#multi').prop('checked', MULTI_COLUMN);
     doLayout();
 }
 
